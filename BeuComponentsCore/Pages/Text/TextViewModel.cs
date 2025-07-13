0febea2bfadba0da45b81eb8e4d277198456ffba
alireza moghanloo <ar.moghanloo368@gmail.com>
2025-07-13 21:08:03 +0330

    ساخت لایه کامپوننت

---
+++ BeuComponentsCore/Pages/Text/TextViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace BeuComponentsCore.Pages.Text
+{
+    public class TextViewModel
+    {
+        public int Id { get; set; }
+        public bool IsFocused { get; set; }
+        public string ComputedClass { get; set; }
+        public bool ShowFloatingLabel { get; set; }
+        public string PlaceHolder { get; set; }
+        public string Type { get; set; }
+        public string Mask { get; set; }
+        public string Content { get; set; }
+    }
+}
