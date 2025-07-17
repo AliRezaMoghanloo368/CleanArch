827168f49e3ebad07188afc075cadb284a165283
alireza moghanloo <ar.moghanloo368@gmail.com>
2025-07-17 12:51:26 +0330

    change design 1

---
+++ CleanArch.Mvc/Controllers/AboutUsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CleanArch.Mvc.Controllers
+{
+    public class AboutUsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
