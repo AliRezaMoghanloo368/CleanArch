4b6174b30ff48e26684735a84e0b576bde1c2b20
alireza moghanloo <ar.moghanloo368@gmail.com>
2025-07-18 14:08:40 +0330

    set cart box

---
+++ CleanArch.Application/ViewModels/CartViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArch.Application.ViewModels
+{
+    public class CartViewModel
+    {
+        public string Title { get; set; }
+        public string FileName { get; set; }
+        public string ToolsName { get; set; }
+        public string Description { get; set; }
+        public float Rate { get; set; }
+    }
+}
