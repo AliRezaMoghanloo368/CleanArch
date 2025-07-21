a663f689efaf4e154a2f31625a8db1c79f82e9fe
alireza moghanloo <ar.moghanloo368@gmail.com>
2025-07-21 21:57:34 +0330

    ایجاد چک باکس جهت نمایش یا عدم نمایش رمز عبور در لاگین یا ثبت نام

--- CleanArch.Mvc/wwwroot/js/site.js
+++ CleanArch.Mvc/wwwroot/js/site.js
@@ -2,3 +2,20 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+function inputPassword() {
+    var x = document.getElementById("input-password");
+    if (x.type === "password") {
+        x.type = "text";
+    } else {
+        x.type = "password";
+    }
+}
+
+function inputRepassword() {
+    var x = document.getElementById("input-repassword");
+    if (x.type === "password") {
+        x.type = "text";
+    } else {
+        x.type = "password";
+    }
+}
