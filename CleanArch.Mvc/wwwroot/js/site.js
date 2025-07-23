64a87200fb13c7551a45c7854a763794868b109e
alireza moghanloo <ar.moghanloo368@gmail.com>
2025-07-23 22:28:06 +0330

    تنظیم آیکنهای رمز عبور

--- CleanArch.Mvc/wwwroot/js/site.js
+++ CleanArch.Mvc/wwwroot/js/site.js
@@ -1,21 +1,23 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
-function inputPassword() {
+﻿function inputPassword() {
     var x = document.getElementById("input-password");
+    var img = document.getElementById("img-password");
     if (x.type === "password") {
         x.type = "text";
+        img.src = "/images/icons/eye-off.svg";  // تصویر چشم بسته یا حالت مخفی
     } else {
         x.type = "password";
+        img.src = "/images/icons/eye.svg";      // تصویر چشم باز یا حالت نمایش
     }
 }
 
 function inputRepassword() {
     var x = document.getElementById("input-repassword");
+    var img = document.getElementById("img-repassword");
     if (x.type === "password") {
         x.type = "text";
+        img.src = "/images/icons/eye-off.svg"; 
     } else {
         x.type = "password";
+        img.src = "/images/icons/eye.svg"; 
     }
 }
